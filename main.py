266bf437db0512c8cb2dbd92b6a0d9b1bfa35ce3
Khalidfurmulie <105319765+Khalidfurmulie@users.noreply.github.com>
2022-12-05 14:21:31 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-import server
-
+from Server import Server
 def print_program_info():
-    # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
 
-# This is the entry point to our program
+    
+    print("Server Automator v0.1 by Justin Ellis")
+
 if __name__ == '__main__':
     print_program_info()
+    # TODO - Create a Server object
+    my_server_ip = "54.189.51.103"
+    my_rsa_key_file = r"C:\Users\khali\.ssh\JE_CNE"
+    username = "ubuntu"
+    my_upgrade_command = 'sudo apt update && sudo apt upgrade -y'
+    my_server = Server(my_server_ip, my_rsa_key_file, username, my_upgrade_command)
+    print(my_server.ping())
+    print("Updating server")
+    ssh_result = my_server.upgrade()
+    print(''.join(ssh_result))
     
-    #The IP address is passed as a string to the Server class in the server.py module     
-    ec2Server = server.Server("54.189.51.103")    
+    
+    
